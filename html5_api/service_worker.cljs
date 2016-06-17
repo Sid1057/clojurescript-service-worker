64fee47e3ade8f241feb929b1cfa4e85ea15ddc0
Ivan Deylid <ivanov.dale@gmail.com>
2016-06-17 17:17:10 +0300

    Create service_worker.cljs

---
+++ html5_api/service_worker.cljs
@@ -0,0 +1,13 @@
+(ns html5-api.service-worker)
+
+(defn is-service-worker-supported?
+	[]
+  (exists? js/navigator.serviceWorker))
+
+(defn register-service-worker
+  [path-to-sw]
+  (if (is-service-worker-supported?)
+      (-> js/navigator
+          .-serviceWorker
+          (.register path-to-sw)
+          (.then #(js/console.log (str 'Service Worker Registered[' path-to-sw ']'))))))
